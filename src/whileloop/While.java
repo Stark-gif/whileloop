ab0d7911c68c3e092d50f407d055cc40ac758775
Stark-gif <ktashfeen35@gmail.com>
2023-11-05 12:17:53 +0530

    whileloop

---
+++ src/whileloop/While.java
@@ -0,0 +1,18 @@
+package whileloop;
+
+public class While {
+	public static void main(String[] args) {
+		System.out.println("hello");
+		System.out.println("hello");
+		System.out.println("hello");
+		System.out.println("hello");
+		System.out.println("hello");
+		System.out.println("hello");
+		System.out.println("hello");
+		System.out.println("hello");
+		System.out.println("hello");
+		System.out.println("hello");
+	}
+	
+	
+}
